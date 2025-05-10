06e18f18af78c84d04a6cc4d662098c046b42c03
JuanzelS <zel.staton@gmail.com>
2025-05-09 20:47:05 -0700

    commit

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, SelectField, SubmitField
+from wtforms import StringField, FloatField, SelectField, SubmitField, PasswordField
 from wtforms_sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, URL, Optional
-
-from grocery_app.models import GroceryStore, GroceryItem
+from wtforms.validators import DataRequired, Length, URL, Optional, ValidationError
+from grocery_app.models import GroceryStore, GroceryItem, User
+from grocery_app import bcrypt
 
 # TODO: Create a form class for new grocery stores
 class GroceryStoreForm(FlaskForm):
@@ -26,3 +26,31 @@ class GroceryItemForm(FlaskForm):
     photo_url = StringField('Photo URL', validators=[URL(), Optional()])
     store = QuerySelectField('Store', query_factory=lambda: GroceryStore.query, get_label='title')
     submit = SubmitField('Submit')
+
+class SignUpForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name',
+        validators=[DataRequired(), Length(min=3, max=50)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if not user:
+            raise ValidationError('No user with that username. Please try again.')
+
+    def validate_password(self, password):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user and not bcrypt.check_password_hash(
+                user.password, password.data):
+            raise ValidationError('Password doesn\'t match. Please try again.')
